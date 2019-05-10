38aa0d9b1ac97d3598e21f050788410b9634c1ae
Benn King <bennkingy@gmail.com>
2019-05-10 21:03:10 +0100

    Updated logo

--- assets/partials/header.php
+++ assets/partials/header.php
@@ -67,7 +67,7 @@
                             <span class="icon-bar"></span>
                             </button>
                             <a class="navbar-brand" href="index.php">
-                                <img src="assets/img/logo/complete-logo-white.png" class="img-responsive" alt="">
+                                <img src="http://www.completeindependentinventories.co.uk/assets/img/Logo/complete-logo-white.png" class="img-responsive" alt="">
                             </a>
                         </div>
                         <!-- Collect the nav links, forms, and other content for toggling -->
