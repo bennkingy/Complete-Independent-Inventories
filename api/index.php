d04a06e0920c55f30e7dfe6bb2928361af2f348f
Benn Dev <bennkingy@gmail.com>
2024-05-08 21:37:39 +0100

    updates

---
+++ api/index.php
@@ -0,0 +1,190 @@
+<?php
+$page = 'index';
+include_once('assets/partials/header.php');
+?>
+<!-- /#header-2 -->
+<section id="bannercontainer" class="bannercontainer">
+    <div class="banner">
+        <ul>
+            <!-- THE 1ST SLIDE -->
+            <li>
+                <img src="assets/img/slides/slide2.jpg" alt="Slide Image">
+                <div class="tp-caption large_bold_white lft ltt"
+                    data-x="47"
+                    data-y="80"
+                    data-speed="300"
+                    data-start="1200"
+                    data-easing="easeOutExpo" style="font-size: 50px; line-height: 50px;">PROPERTY INVENTORY<br/> SPECIALIST</div>
+                <div class="tp-caption small_light_white sfl stl"
+                    data-x="50"
+                    data-y="185"
+                    data-speed="600"
+                    data-start="1400"
+                    data-easing="easeOutExpo">
+                    We offer professional property inventories for landlords<br/>and letting agents supported by high quality photo evidence.<br/>We eliminate all possible disputes between tenant and landlord<br/>about the condition of the property before and after the tenancy.
+                </div>
+                <a href="" class="caption btn btn-flat flat-primary btn-bordered sfb stb" 
+                    data-x="50"
+                    data-y="320"
+                    data-speed="300" 
+                    data-start="1600"
+                    data-easing="easeOutExpo">Learn More</a>
+                <a href="" class="caption btn btn-flat flat-color sfb stb"  
+                    data-x="170" 
+                    data-y="320" 
+                    data-speed="300"
+                    data-start="1800"
+                    data-easing="easeOutExpo">Purchase Now</a>
+                <div class="caption lfr ltr"
+                    data-x="725"
+                    data-y="50"
+                    data-speed="300"
+                    data-start="2000"
+                    data-easing="easeOutExpo"><img src="assets/img/keys.png" width="350" height="350" alt="Slide Image"></div>
+            </li>
+            <!--THE 2ND SLIDE 
+            <li data-transition="slideleft" data-slotamount="10" data-masterspeed="300">
+                <img src="assets/img/slides/slide2.jpg" alt="Slide Image">
+                <div class="tp-caption large_bold_white lft ltt"
+                    data-x="47"
+                    data-y="80"
+                    data-speed="300"
+                    data-start="1200"
+                    data-easing="easeOutExpo" style="font-size: 50px; line-height: 50px;">SUPPORT <br> VIDEO</div>
+                <div class="tp-caption small_light_white sfl stl"
+                    data-x="50"
+                    data-y="185"
+                    data-speed="600"
+                    data-start="1400"
+                    data-easing="easeOutExpo">
+                    Lorem ipsum dolor sit amet, eum cu sint dolores, dicat<br>
+                    possit malorum at sit, ut natum assentior mea. Mel<br>
+                    congue malorum te, eu cum blandit salutatus comprehensam<br> 
+                    atqui causae ancillae cum ei.
+                </div>
+                <a href="" class="caption btn btn-flat flat-primary btn-bordered sfb stb" 
+                    data-x="50"
+                    data-y="320"
+                    data-speed="300" 
+                    data-start="1600"
+                    data-easing="easeOutExpo">Learn More</a>
+                <a href="" class="caption btn btn-flat flat-color sfb stb"  
+                    data-x="170" 
+                    data-y="320" 
+                    data-speed="300"
+                    data-start="1800"
+                    data-easing="easeOutExpo">Purchase Now</a>
+                <div class="caption lfr ltr"
+                    data-x="590"
+                    data-y="90"
+                    data-speed="300"
+                    data-start="2000"
+                    data-easing="easeOutExpo"><iframe src="https://player.vimeo.com/video/25708134?title=0&amp;byline=0&amp;portrait=0" width="460" height="259"></iframe></div>
+            </li>
+            THE 3RD SLIDE
+            <li data-transition="boxslide" data-slotamount="10" data-masterspeed="300">
+                <img src="assets/img/slides/slide3.jpg" alt="Slide Image">
+                <div class="tp-caption large_bold_white lft ltt"
+                    data-x="47"
+                    data-y="80"
+                    data-speed="300"
+                    data-start="1200"
+                    data-easing="easeOutExpo" style="font-size: 50px; line-height: 50px;">MODIFY <br> EASILY</div>
+                <div class="tp-caption small_light_white sfl stl"
+                    data-x="50"
+                    data-y="185"
+                    data-speed="600"
+                    data-start="1400"
+                    data-easing="easeOutExpo">
+                    Lorem ipsum dolor sit amet, eum cu sint dolores, dicat<br>
+                    possit malorum at sit, ut natum assentior mea. Mel<br>
+                    congue malorum te, eu cum blandit salutatus comprehensam<br> 
+                    atqui causae ancillae cum ei.
+                </div>
+                <a href="" class="caption btn btn-flat flat-primary btn-bordered sfb stb" 
+                    data-x="50"
+                    data-y="320"
+                    data-speed="300" 
+                    data-start="1600"
+                    data-easing="easeOutExpo">Learn More</a>
+                <a href="" class="caption btn btn-flat flat-color sfb stb"  
+                    data-x="170" 
+                    data-y="320" 
+                    data-speed="300"
+                    data-start="1800"
+                    data-easing="easeOutExpo">Purchase Now</a>
+                <div class="caption lfr ltr"
+                    data-x="675"
+                    data-y="25"
+                    data-speed="300"
+                    data-start="2000"
+                    data-easing="easeOutExpo"><img src="assets/img/slides/imac.png" alt="Slide Image"></div>
+            </li> -->
+        </ul>
+        <div class="tp-bannertimer tp-bottom"></div>
+    </div>
+    <!-- /.banner -->
+</section>
+<!-- /.bannercontainer -->
+<section id="our-services" class="section colored-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-6 wrapper-1">
+                <div class="service service-4">
+                    <span class="service-icon">
+                    <i class="icon-file-text"></i>
+                    </span>
+                    <h4>Inventories</h4>
+                    <p>This is a professional up to date detailed inventory report of the condition of the furnishings, fixtures & fixings of the property, including, walls, woodwork, flooring, carpets, glazing, lights & fittings, bathroom & kitchens, radiators, gardens & sheds & outbuildings. photos are attached to the inventory report.
+                   </p>
+                </div>
+            </div>
+            <!-- /.col-sm-6 -->
+            <div class="col-xs-12 col-md-6 wrapper-1">
+                <div class="service service-4">
+                    <span class="service-icon">
+                    <i class="icon-foursquare"></i>
+                    </span>
+                    <h4>Tenant Check In</h4>
+                    <p>A check in compares the original inventory against the contents & condition on the day the tenancy starts at check in, we will produce a detailed check in report including the condition of cleaning & meters & keys & photos, tenants will agree the check in & sign the check in summary report. photos will be attached.</p>
+                </div>
+            </div>
+            <!-- /.col-sm-6 -->
+            <div class="col-xs-12 col-md-6 wrapper-1">
+                <div class="service service-4">
+                    <span class="service-icon">
+                    <i class="icon-long-arrow-right"></i>
+                    </span>
+                    <h4>Check Out Report</h4>
+                    <p>At the end of tenancy a full check out report will be completed & both the original inventory & check in will be used together,  meter readings will be take & keys collected & returned back to the agent, a full check out report will be compiled including any recommended costs to the tenants. photos will be taken.</p>
+                </div>
+            </div>
+            <!-- /.col-sm-6 -->
+            <div class="col-xs-12 col-md-6 wrapper-1">
+                <div class="service service-4">
+                    <span class="service-icon">
+                    <i class="icon-bullseye"></i>
+                    </span>
+                    <h4>Interim Inspections</h4>
+                    <p>Property inspections are carried out on request at properties, for landlords & agents a report will be sent to the agent or landlord with findings & photos & maintenance issues recommended.</p>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</section>
+<!-- /#our-services 
+<section id="awesome-callout" class="section colored-wrapper">
+    <div class="container">
+        <div class="awesome-callout">
+            <h2>Lorem ipsum dolor sit amet?</h2>
+            <p>Pri iriure feugiat ut, nihil utroque ei mei. Eum primis causae scripserit ad, decore inimicus periculis vix id. Pri aeterno nostrud maiestatis ne, assum nonumes eam ne.</p>
+            <a href="#" class="btn btn-flat flat-color btn-lg">Purchase Now</a>
+        </div>
+    </div>
+</section>
+#awesome-callout -->
+<?php
+include_once('assets/partials/footer.php');
+?>
